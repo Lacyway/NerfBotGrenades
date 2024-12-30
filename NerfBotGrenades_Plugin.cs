85aadb9ed5684b902c578a18db2be488ea32e6f7
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-12-30 08:30:55 +0100

    Add project files.

---
+++ NerfBotGrenades_Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+
+namespace NerfBotGrenades
+{
+	[BepInPlugin("com.lacyway.nbg", "NerfBotGrenades", "1.0.0")]
+	internal class NerfBotGrenades_Plugin : BaseUnityPlugin
+	{
+		internal static ManualLogSource PluginLogger;
+		internal static ConfigEntry<float> Multiplier;
+
+		protected void Awake()
+		{
+			PluginLogger = Logger;
+			PluginLogger.LogInfo($"{nameof(NerfBotGrenades_Plugin)} has been loaded.");
+
+			Multiplier = Config.Bind("1. Settings", "Multiplier", 0f, new ConfigDescription("The multiplier to bots inaccuracy with grenades",
+				new AcceptableValueRange<float>(0f, 10f)));
+
+			new BotGrenadeController_ToThrowDirection_Patch().Enable();
+		}
+	}
+}
---
+++ Patches/BotGrenadeController_ToThrowDirection_Patch.cs
@@ -0,0 +1,27 @@
+﻿using SPT.Reflection.Patching;
+using System.Reflection;
+using UnityEngine;
+
+namespace NerfBotGrenades
+{
+	internal class BotGrenadeController_ToThrowDirection_Patch : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(BotGrenadeController).GetProperty(nameof(BotGrenadeController.ToThrowDirection)).GetGetMethod();
+		}
+
+		[PatchPrefix]
+		public static bool Prefix(ref Vector3 __result, BotGrenadeController __instance, Vector3 ____precisionOffset)
+		{
+#if DEBUG
+			Vector3 original = __instance.AIGreanageThrowData.Direction + ____precisionOffset;
+#endif
+			__result = __instance.AIGreanageThrowData.Direction + (____precisionOffset * NerfBotGrenades_Plugin.Multiplier.Value);
+#if DEBUG
+			NerfBotGrenades_Plugin.PluginLogger.LogWarning($"Modified grenade result: {__result}, original: {original}");
+#endif
+			return false;
+		}
+	}
+}
